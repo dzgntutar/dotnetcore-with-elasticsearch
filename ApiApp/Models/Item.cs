cf8dc8274bf81b91e16f9305dd20e1e20371f806
dutarrr <duzguntutar@gmail.com>
2021-08-25 13:14:13 +0300

    Item example

---
+++ ApiApp/Models/Item.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ApiApp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build-env
 COPY ApiApp/ App/
 WORKDIR /App
-ENV ASPNETCORE_URLS=https://localhost:5001;http://localhost:5000
+ENV ASPNETCORE_URLS=http://+:80
 RUN dotnet restore
 RUN dotnet publish -c Release -o publish
 ENTRYPOINT ["dotnet", "./publish/ApiApp.dll"]
